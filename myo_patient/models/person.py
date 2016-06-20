b77eec689ad7237add98648dd82ff1c0f2a5d1c6
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-06-20 09:51:35 -0300

    [ADD] myo_patient

---
+++ myo_patient/models/person.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+from openerp import fields, models
+
+
+class Person(models.Model):
+    _inherit = 'myo.person'
+
+    code = fields.Char(size=64, string='Person/Patient Code', required=False)
+    is_patient = fields.Boolean('Is Patient',
+                                help="If checked, the Person is a Patient.",
+                                default=0)
