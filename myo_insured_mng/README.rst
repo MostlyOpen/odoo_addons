9c857ad9b3dfc63bc79c7cf0b9c673b9738c6bc7
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-06-23 08:43:02 -0300

    [ADD] myo_insured_mng

---
+++ myo_insured_mng/README.rst
@@ -0,0 +1,6 @@
+Insured Management Module
+=========================
+
+Insured Management Module used in MostlyOpen Solutions.
+
+Developed by `CLVsol <https://clvsol.com>`_.
---
+++ myo_insured_mng/__openerp__.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    'name': 'Insured Management',
+    'summary': 'Insured Management Module used in MostlyOpen Solutions.',
+    'version': '2.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'http://mostlyopen.org',
+    'images': [],
+    'depends': [
+        'myo_base',
+        # 'myo_tag',
+        # 'myo_annotation',
+        # 'myo_address',
+        # 'myo_insured',
+        # 'myo_insured_card',
+    ],
+    'data': [
+        'security/insured_mng_security.xml',
+        'security/ir.model.access.csv',
+        'views/insured_mng_view.xml',
+        # 'views/insured_category_view.xml',
+        # 'views/tag_view.xml',
+        # 'views/annotation_view.xml',
+        # 'views/insurance_view.xml',
+        # 'views/insurance_client_view.xml',
+        # 'views/insured_mng_view.xml',
+        # 'views/insured_mng_view.xml',
+        # 'views/insured_mng_relation_view.xml',
+        'views/insured_mng_menu_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
