b513b426450d972ec2638077a7fdeae01ce31a7f
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-10-11 15:25:43 -0300

    [IMP] myo_address

--- myo_address/models/address_name.py
+++ myo_address/models/address_name.py
@@ -37,25 +37,27 @@ class Address(models.Model):
 
     @api.depends('street', 'street2')
     def _get_suggested_name(self):
-        if self.street:
-            self.suggested_name = self.street
-            if self.street2:
-                self.suggested_name = self.suggested_name + ' - ' + self.street2
-        else:
-            if not self.suggested_name:
-                if self.code:
-                    self.suggested_name = self.code
+        for record in self:
+            if record.street:
+                record.suggested_name = record.street
+                if record.street2:
+                    record.suggested_name = record.suggested_name + ' - ' + record.street2
+            else:
+                if not record.suggested_name:
+                    if record.code:
+                        record.suggested_name = record.code
 
     @api.multi
     def write(self, values):
         ret = super(Address, self).write(values)
-        if self.automatic_set_name:
-            if self.name != self.suggested_name:
-                values['name'] = self.suggested_name
-                return super(Address, self).write(values)
-        else:
-            if ('name' in values and values['name'] == '/') or \
-               (self.name == '/'):
-                values['name'] = self.suggested_name
-                return super(Address, self).write(values)
+        for record in self:
+            if record.automatic_set_name:
+                if record.name != record.suggested_name:
+                    values['name'] = record.suggested_name
+                    super(Address, record).write(values)
+            else:
+                if ('name' in values and values['name'] == '/') or \
+                   (record.name == '/'):
+                    values['name'] = record.suggested_name
+                    super(Address, record).write(values)
         return ret
