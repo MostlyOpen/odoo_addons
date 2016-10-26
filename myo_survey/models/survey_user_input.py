9b1848e23a69462ae7560833322d8c9ae914f9ff
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-10-26 11:40:25 -0200

    [IMP] myo_survey

--- myo_survey/models/survey_user_input.py
+++ myo_survey/models/survey_user_input.py
@@ -18,10 +18,52 @@
 #
 ###############################################################################
 
-from openerp import fields, models
+from openerp import api, fields, models
 
 
 class SurveyUserInput(models.Model):
     _inherit = 'survey.user_input'
 
-    external_code = fields.Char('External Code', help="External Code")
+    active = fields.Boolean(
+        'Active',
+        help="If unchecked, it will allow you to hide the survey user input without removing it.",
+        default=1
+    )
+    link_survey_user_input = fields.Boolean(
+        'Link Survey User Input',
+        help="",
+        default=0
+    )
+    linked_code = fields.Char('Linked Code', help="Linked Code")
+    linked_message = fields.Char('Linked Message', help="Linked Message")
+    linked_state = fields.Selection(
+        [('draft', 'Draft'),
+         ('revised', 'Revised'),
+         ('waiting', 'Waiting'),
+         ('linked', 'Linked'),
+         ('canceled', 'Canceled')
+         ], string='Linkded State', default='draft', readonly=False, required=False, help=""
+    )
+
+    @api.multi
+    def write(self, values):
+        if 'link_survey_user_input' in values:
+            del values['link_survey_user_input']
+
+            for survey_user_input in self:
+                if survey_user_input.linked_state == 'draft':
+                    if len(survey_user_input.user_input_line_ids) > 0:
+                        linked_code = survey_user_input.user_input_line_ids[0].value_text
+                        values['linked_code'] = linked_code
+                        values['linked_state'] = 'revised'
+                        document_model = self.env['myo.document']
+                        document = document_model.search([('code', '=', linked_code), ])
+                        if document.id != []:
+                            document.survey_user_input_id = survey_user_input.id
+                            document.state = 'done'
+                            values['linked_state'] = 'linked'
+                    super(SurveyUserInput, survey_user_input).write(values)
+            return True
+
+        else:
+            return super(SurveyUserInput, self).write(values)
