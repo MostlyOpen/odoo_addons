b91fca50beb0e9285e475215ae5fa29df11e0aef
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-10-27 17:48:31 -0200

    [IMP] myo_lab_test

--- myo_lab_test/wizard/create_lab_test.py
+++ myo_lab_test/wizard/create_lab_test.py
@@ -18,41 +18,40 @@
 #
 ###############################################################################
 
-from openerp.osv import osv
+from openerp import api, fields, models
 
 
-class CreateLabTest(osv.osv_memory):
+class CreateLabTest(models.TransientModel):
     _name = 'myo.lab_test.create'
 
-    def create_lab_test(self, cr, uid, ids, context={}):
+    lab_test_patient_ids = fields.Many2many('myo.lab_test.patient', string='Lab Test Patients')
 
-        # data = ids
+    @api.multi
+    def create_lab_test(self):
 
-        print('>>>>>', self, ids, context.get('active_id'))
-        test_request_obj = self.pool.get('myo.lab_test.patient')
-        lab_obj = self.pool.get('myo.lab_test')
+        self.lab_test_patient_ids = self._context.get('active_ids')
+        lab_obj = self.env['myo.lab_test']
 
-        test_report_data = {}
-        test_cases = []
-        test_obj = test_request_obj.browse(cr, uid, context.get('active_id'), context=context)
-        if test_obj.state == 'tested':
-            raise osv.except_osv(('UserError'), ('Test Report already created.'))
-        test_report_data['lab_test_type_id'] = test_obj.name.id
-        test_report_data['patient_id'] = test_obj.patient_id.id
-        # test_report_data['requestor'] = test_obj.doctor_id.id
-        test_report_data['date_requested'] = test_obj.date
+        for test_obj in self.lab_test_patient_ids:
+            test_report_data = {}
+            test_cases = []
+            if test_obj.state == 'draft':
+                test_report_data['lab_test_type_id'] = test_obj.name.id
+                test_report_data['patient_id'] = test_obj.patient_id.id
+                test_report_data['date_requested'] = test_obj.date
 
-        for criterion in test_obj.name.criterion_ids:
-            test_cases.append((0, 0, {'name': criterion.name,
-                                      'sequence': criterion.sequence,
-                                      'normal_range': criterion.normal_range,
-                                      'unit_id': criterion.unit_id.id
-                                      }))
-        test_report_data['criterion_ids'] = test_cases
-        lab_id = lab_obj.create(cr, uid, test_report_data, context=context)
-        test_request_obj.write(cr, uid, context.get('active_id'), {'state': 'tested', 'lab_test_id': lab_id})
+                for criterion in test_obj.name.criterion_ids:
+                    test_cases.append((0, 0, {'name': criterion.name,
+                                              'sequence': criterion.sequence,
+                                              'normal_range': criterion.normal_range,
+                                              'unit_id': criterion.unit_id.id
+                                              }))
+                test_report_data['criterion_ids'] = test_cases
+                lab_id = lab_obj.create(test_report_data)
+                test_obj.state = 'tested'
+                test_obj.lab_test_id = lab_id
         return {
-            'domain': "[('id','=', " + str(lab_id) + ")]",
+            'domain': "[]",
             'name': 'Lab Test Report',
             'view_type': 'form',
             'view_mode': 'tree,form',
